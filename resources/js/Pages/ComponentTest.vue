8ea38fcfc038db7a57f5ff3c1b736ae8f124638d
aaattt25 <p.aaattt25@gmail.com>
2024-05-01 22:54:58 +0900

    done Section2-35

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+
+const emitTest = e =>{console.log(e)}
+</script>
+
+<template>
+  <GuestLayout>
+    こっちが親コンポーネントだよ
+    <Label value="件名">タイトル</Label>
+    <TextInput
+      modelValue="初期値です"
+      @update:modelValue="emitTest"
+    ></TextInput>
+  </GuestLayout>
+</template>
